229bfaec4019ae6847ee0fdf33717d48ad4e4d4c
Artem Kuchumov <duketemon@gmail.com>
2020-01-19 15:12:34 +0100

    Updated tests for dummy features generators

--- tests/dummy_features_tests.py
+++ tests/dummy_features_tests.py
@@ -6,14 +6,15 @@ from fet import generate_dummy_features_by_columns
 from fet import generate_dummy_features_by_data_type
 from fet import generate_dummy_features_by_column_prefix
 
+
 # TESTS FOR generate_dummy_features_by_columns
-
-
 def generate_dummy_features_by_columns__one_column_test():
-    data = {'cat': ['val-1', 'val-1', 'val-2']}
-    df = pd.DataFrame(data=data)
-    df = generate_dummy_features_by_columns(df, ['cat'])
-    assert np.array_equal(np.array(['cat_val-1', 'cat_val-2']), df.columns)
+    df = pd.DataFrame(data={'platform': ['web', 'android', 'ios', 'other']})
+    df = generate_dummy_features_by_columns(df, ['platform'])
+    assert np.array_equal(
+        np.array(sorted(['platform_web', 'platform_android', 'platform_ios', 'platform_other'])),
+        sorted(df.columns)
+    )
 
 
 def generate_dummy_features_by_columns__two_columns_test():
@@ -32,7 +33,18 @@ def generate_dummy_features_by_columns__wrong_key_test():
         generate_dummy_features_by_columns(df, ['catw'])
 
 
-# TESTS FOR generate_dummy_features_by_columns
+# TESTS FOR generate_dummy_features_by_data_type
+def generate_dummy_features_by_data_type__one_column_test():
+    df = pd.DataFrame(data={
+        'id': [1, 2, 3, 4],
+        'platform': ['web', 'android', 'ios', 'other']
+    })
+    df = generate_dummy_features_by_data_type(df, 'object')
+    assert np.array_equal(
+        np.array(sorted(['id', 'platform_web', 'platform_android', 'platform_ios', 'platform_other'])),
+        sorted(df.columns)
+    )
+
 
 def generate_dummy_features_by_data_type__two_object_columns_test():
     df = pd.DataFrame(data={
@@ -44,7 +56,20 @@ def generate_dummy_features_by_data_type__two_object_columns_test():
     assert np.array_equal(np.array(['cat-2', 'cat-1_val-1', 'cat-1_val-2', 'cat-3_val-1', 'cat-3_val-2']), df.columns)
 
 
-# TESTS FOR generate_dummy_features_by_columns
+# TESTS FOR generate_dummy_features_by_column_prefix
+def generate_dummy_features_by_column_prefix__one_column_test():
+    df = pd.DataFrame(data={
+        'id': [1, 2, 3, 4],
+        'user-platform': ['web', 'android', 'ios', 'other'],
+        'user-plan': ['free', 'premium', 'free', 'free'],
+    })
+    df = generate_dummy_features_by_column_prefix(df, 'user-')
+    print(df.head())
+    assert np.array_equal(
+        np.array(sorted(['id', 'user-plan_free', 'user-plan_premium',
+                         'user-platform_web', 'user-platform_android', 'user-platform_ios', 'user-platform_other'])),
+        sorted(df.columns)
+    )
 
 
 def generate_dummy_features_by_column_prefix__two_object_columns_test():
@@ -62,6 +87,8 @@ if __name__ == '__main__':
     generate_dummy_features_by_columns__two_columns_test()
     generate_dummy_features_by_columns__wrong_key_test()
 
+    generate_dummy_features_by_data_type__one_column_test()
     generate_dummy_features_by_data_type__two_object_columns_test()
 
+    generate_dummy_features_by_column_prefix__one_column_test()
     generate_dummy_features_by_column_prefix__two_object_columns_test()
